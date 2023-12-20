74d5574abbea23f2cbc965532fd2eda9a9f40a8d
carlos H <carlitobr730@yahoo.com>
2023-12-20 15:34:22 -0400

    exercicio 251 e 252 adicionados

---
+++ exercicios/251.js
@@ -0,0 +1,7 @@
+const doar = parseInt(prompt(`digite sua idade`))
+
+if (doar >= 18 && doar <= 67) {
+    alert(`Ja pode doar sangue`)
+}else{
+    alert(`Nao pode doar sangue`)
+}
---
+++ exercicios/252.js
@@ -0,0 +1,13 @@
+let n1 = parseInt(prompt(`Digite um n`))
+let n2 = parseInt(prompt(`Digite um outro n`))
+if (n1 > n2) {
+    alert(`n1 é o maior`);
+} else if (n2 > n1) {
+    alert(`n2 é o maior`);
+}
+
+if (n1 < n2) {
+    alert(`n1 é o menor`);
+} else if (n2 < n1) {
+    alert(`n2 é o menor`);
+}
