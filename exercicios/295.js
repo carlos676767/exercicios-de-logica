630e77a3501010f741dc22f6fb47c6dd6e284034
carlos H <carlitobr730@yahoo.com>
2024-01-07 16:19:39 -0400

    adicionados novos exercicios

---
+++ exercicios/295.js
@@ -0,0 +1,27 @@
+let vetor = [];
+const vetores = () => {
+  for (let i = 0; i <= 20; i++) {
+    let numero = parseInt(prompt(`Digite um valor`));
+    if (numero > 0) {
+      vetor.push(numero);
+    }
+  }
+  console.log(vetor);
+};
+
+vetores();
+
+const mexerValores = () => {
+  let maior = 0;
+  let indice = 0;
+  for (let i = 0; i < vetor.length; i++) {
+    if (vetor[i] > maior) {
+      maior = vetor[i];
+      indice = i;
+    }
+  }
+  console.log(`o maior valor e ${maior}`);
+  console.log(`o indice do elemento e ${indice}`);
+};
+
+mexerValores();
