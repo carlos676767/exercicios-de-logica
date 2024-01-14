077a0aa8b6b4fea1c66d1d6dcf80805e45a2f913
carlos H <carlitobr730@yahoo.com>
2024-01-14 12:14:48 -0400

    adicionados novos exercicios

---
+++ exercicios/303.js
@@ -0,0 +1,5 @@
+const array = [99,8,3]
+console.log(`array com o valor original`, array);
+let remover = array.pop()
+console.log(`valor removido`, remover);
+console.log(`array com o valor resmovido`, array);
