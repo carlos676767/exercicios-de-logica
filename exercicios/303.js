14239d56b5502dba465d0260f91070b6c3601873
carlos H <carlitobr730@yahoo.com>
2024-01-14 12:26:32 -0400

    adicionados novos exercicios

--- exercicios/303.js
+++ exercicios/303.js
@@ -1,3 +1,4 @@
+//removendo o ultimo item de um array
 const array = [99,8,3]
 console.log(`array com o valor original`, array);
 let remover = array.pop()
---
+++ exercicios/304.js
@@ -0,0 +1,3 @@
+const array = [2, 10, 80, 4];
+let add = array.unshift(100,200)
+console.log(array);
