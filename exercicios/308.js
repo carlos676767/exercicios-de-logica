76e02ad5bfe82c68d953fec9a4327ca2218af911
carlos676767 <carlitobr730@gmail.com>
2024-01-19 09:28:48 -0800

    adicionados novos exercicios~

---
+++ exercicios/308.js
@@ -0,0 +1,9 @@
+const array = [`carlos`, `ana`, `silva`];
+let indice = array.indexOf(`carlos`);
+console.log(indice);
+
+if (indice !== -1) {
+  console.log(`elemento encontrado`);
+} else {
+  console.log(`elemento nao encontrado`);
+}
