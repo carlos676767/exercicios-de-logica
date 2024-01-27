98953b672dbbbd59eb43f2b12d0f56f7f21b4aca
carlos676767 <carlitobr730@gmail.com>
2024-01-27 09:08:30 -0800

    novos exercicios

---
+++ exercicios/323.ts
@@ -0,0 +1,15 @@
+let lista: number[] = [5,4,3,2,1]
+const doubleSort = (list: number[]) => {
+  for (let i = 0; i < list.length; i++) {
+    for (let j = 0; j < list.length; j++) {
+      if(list[i] < list[j]){
+        const temp: number = list[i]
+        list[i] = list[j]
+        list[j] = temp
+      }
+    }
+  }
+  return list
+}
+
+console.log(doubleSort(lista))
