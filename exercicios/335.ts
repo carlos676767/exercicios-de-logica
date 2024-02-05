9bbf8dd36155d2bc78a9fcf0dce0b31eeadadcdd
carlos676767 <carlitobr730@gmail.com>
2024-02-05 12:16:32 -0800

    novos exercicios

---
+++ exercicios/335.ts
@@ -0,0 +1,8 @@
+/*Exercício: Troca de Valores
+Escreva um programa que solicite ao usuário que insira dois números e, em seguida, 
+troque os valores desses números. Ou seja, se o usuário inserir 5 e 8, o programa 
+deve exibir que os números foram trocados, 
+e agora a variável que originalmente continha 5 agora contém 8, e vice-versa*/
+
+const n1: number[] = [1,2].reverse()
+console.log(n1)
