5792786b53c2f483d88b995ab028df543439489f
carlos676767 <carlitobr730@gmail.com>
2024-02-08 07:54:32 -0800

    novos exercicios

---
+++ exercicios/339.js
@@ -0,0 +1,4 @@
+const valorSalarioUsuario = parseInt(prompt(`Digite o valor do salario do usuario.`))
+const salarioMinimo = 1412
+const salarioCalcular = valorSalarioUsuario/ salarioMinimo
+alert(`voce recebe ${salarioCalcular.toFixed(2)} salarios minimos no total`)
