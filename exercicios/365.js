fa0c1b5fcc474893c281f70661adfcc9762001c6
carlos676767 <carlitobr730@gmail.com>
2024-02-19 09:40:33 -0800

    novos exercicios

--- exercicios/365.js
+++ exercicios/365.js
@@ -1,6 +1,2 @@
-//invertendo um array sem mudar o valor dele.
-const array = [10,9,8,7,6,5,4,3,2,1]
-const inverterArray = array.toReversed();
-console.log(inverterArray);
-console.log(array);
+
 
---
+++ exercicios/366.js
@@ -0,0 +1,12 @@
+//metodo at 
+
+const array = [4,25,4,8]
+const usarAt = array.at(2)
+console.log(usarAt)
+
+
+//indexOf 
+
+const myNumeros = [5,1,7,9,2]
+const usarIdexOf = myNumeros.indexOf(1)
+console.log(usarIdexOf)
