5183573c4d690b2d0724fac809b31285eb452df0
carlos676767 <carlitobr730@gmail.com>
2024-03-16 11:01:25 -0700

    novos exercicios

---
+++ exercicios/400.ts
@@ -0,0 +1,17 @@
+//Destructuring, basicamente e uma maneira mais simples de acessar um objeto.
+
+interface Pessoa {
+    nome: string
+    idade: string
+    estado: string
+}
+
+const pessoa: Pessoa = {
+    nome: 'carlos',
+    idade: "25",
+    estado: "mato grosso"
+}
+
+
+const {nome, idade, estado} = pessoa
+console.log(nome);
