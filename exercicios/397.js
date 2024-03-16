7d8569f75bd304e82256ea568a1a6687111b05e5
carlos676767 <carlitobr730@gmail.com>
2024-03-16 09:07:32 -0700

    novos exercicios

---
+++ exercicios/397.js
@@ -0,0 +1,21 @@
+// // You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or
+//  other items. We want to create the text that should be displayed next to such an item.
+
+// // Implement the function which takes an array containing the names of people that like an item. It must return the display 
+// text as shown in the examples:
+
+// // []                                -->  "no one likes this"
+// // ["Peter"]                         -->  "Peter likes this"
+// // ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// // ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// // Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+function likes(names) {
+  console.log(`${names[0]} likes this`);
+  console.log(`${names[1]} and ${names[2]}like this`);
+  console.log(`$[] no one likes this`);
+  return names;
+}
+const names = ["Alex", "Jacob", "Mark", "Max"];
+likes(names);
