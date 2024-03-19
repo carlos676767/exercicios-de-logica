ab549e60b1a2e4e3312e90d47978e203834b5d3c
carlos676767 <carlitobr730@gmail.com>
2024-03-19 07:41:14 -0700

    exercicio da codewars

---
+++ exercicios/404.ts
@@ -0,0 +1,8 @@
+function createPhoneNumber(numbers: number[]){
+    const converterEmString = numbers.join("")
+    const telefoneRgex = new RegExp(/^(\d{3})(\d{3})(\d{4})$/)
+    const converter = converterEmString.replace(telefoneRgex, "($1) $2-$3")
+    return converter
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
