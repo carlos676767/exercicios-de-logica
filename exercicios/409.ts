617a72fbf4fcba1d193660f11455913500f9f2d6
carlos676767 <carlitobr730@gmail.com>
2024-03-27 09:15:02 -0700

    novos exercicios

---
+++ exercicios/409.ts
@@ -0,0 +1,23 @@
+const doubleSort = (numero: string) => {
+    const valores: string = numero
+    const transformandoEmArray = valores.split("")
+    for (let i = 0; i < transformandoEmArray.length; ++i) {
+      for (let j = 0; j < transformandoEmArray.length; j++) {
+        if (transformandoEmArray[i] < transformandoEmArray[j]) {
+          let temp = transformandoEmArray[i]
+          transformandoEmArray[i] = transformandoEmArray[j]
+          transformandoEmArray[j] = temp
+        }
+      }
+    }
+    let pegarArrayConvertida: number[] = []
+    const converterStringEmNumero = () => {
+      for (let j = 0; j < transformandoEmArray.length; j++) {
+        pegarArrayConvertida.push(parseInt(transformandoEmArray[j]))
+      }
+    }
+    converterStringEmNumero()
+    return pegarArrayConvertida
+  }
+  console.log(doubleSort("52813"))
+  
