ecd051209068c0a35b2f5dc8efdc254a19e9682f
carlos676767 <carlitobr730@gmail.com>
2024-04-09 09:04:24 -0700

    .

--- exercicios/417.ts
+++ exercicios/417.ts
@@ -1,15 +1,15 @@
-const array: number[] = [1, 2, 3, 4, 5]
-const pegarValores: number[] = []
+const array: number[] = [1, 2, 3, 4, 5];
+const pegarValores: number[] = [];
 
-let valor: number = array[0]
+let valor: number = array[0];
 
 for (let j = 0; j < array.length; j++) {
-    if(valor < array[j]){
-        valor = array[j]
-    }
-    if(array[j] < valor){
-        valor = array[j]
-    }
+  if (valor < array[j]) {
+    valor = array[j];
+  }
+  if (array[j] < valor) {
+    valor = array[j];
+  }
 }
 
-console.log(valor)
+console.log(valor);
