f4fde436aac88fd8e01b25a8fc200afff128c8a8
carlos676767 <carlitobr730@gmail.com>
2024-04-22 07:22:32 -0700

    novo exercicio

---
+++ exercicios/429.ts
@@ -0,0 +1,17 @@
+// Dada uma string composta por
+// palavras e espaços retorne o comprimento da
+// última palavra, ignorando os espacos.
+
+
+const retornarComprimentoDaUltimaString = (palavra: string) => {
+  const transformarEmArray: string[] = palavra.split(" ");
+  const obterUltimaString = transformarEmArray.pop();
+  const obterComprimentoString =obterUltimaString !== undefined ? obterUltimaString.length : null;
+  return obterComprimentoString;
+};
+
+console.log(
+  retornarComprimentoDaUltimaString(
+    "Esta    é    uma     string     com         espaços"
+  )
+);
