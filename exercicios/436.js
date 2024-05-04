69f4bfbdd6e0c8f874883e6fbd7d2761137a6a71
carlos676767 <carlitobr730@gmail.com>
2024-05-03 19:11:07 -0700

    .

--- exercicios/436.js
+++ exercicios/436.js
@@ -1,5 +1,4 @@
-const array = [1,2,3,4,5,6,7,8,9,10]
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-
-const pares = array.filter(arr => arr %2 == 0)
+const pares = array.filter((arr) => arr % 2 == 0);
 console.log(pares);
