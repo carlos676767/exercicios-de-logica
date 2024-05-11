bb8c87cf7f5c819a73403d2128aac45f01a1f04e
carlos676767 <carlitobr730@gmail.com>
2024-05-11 13:34:55 -0700

    novos exercicios

---
+++ exercicios/449.ts
@@ -0,0 +1,4 @@
+const salariofuncionario = Number(prompt("salario funcionario"));
+const salariominimo = Number(prompt("salario minimo"));
+const qtdsalariosminimos = salariofuncionario / salariominimo;
+alert(salariominimo)
