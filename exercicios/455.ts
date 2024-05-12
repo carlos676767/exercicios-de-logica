b220d9c06fe9dce66ca5ae847eafbc2f9d87a431
carlos676767 <carlitobr730@gmail.com>
2024-05-12 16:05:28 -0700

    .

--- exercicios/455.ts
+++ exercicios/455.ts
@@ -2,12 +2,10 @@
 //verificar se o valor e igual negativo
 //pego a posicao do meu elemento e troco ele por 0
 const vetor: number[] = [10, -4, 20, -5, 2, -21];
-let remover;
 for (let i = 0; i < vetor.length; i++) {
   if (vetor[i] < 0) {
-    remover = vetor.splice(i, 1, 0);
+    vetor.splice(i, 1, 0);
   }
 }
 
-console.log(remover);
 console.log(vetor);
---
+++ exercicios/456.ts
@@ -0,0 +1,6 @@
+//refaturando codigo
+const vetorValores: number[] = [10, -4, 20, -5, 2, -21]
+const vetorNovo = vetorValores.map(numero =>   numero < 0 ? 0 : numero);
+console.log(vetorNovo)
+
+
