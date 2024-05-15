5802c478a481d1394113868a24b09f9274007d7a
carlos676767 <carlitobr730@gmail.com>
2024-05-15 08:45:24 -0700

    .

---
+++ exercicios/464.js
@@ -0,0 +1,9 @@
+const mapa = new Map();
+mapa.set("nome", "silva");
+mapa.set("idade", 30);
+mapa.set("");
+console.log(mapa);
+
+const array = [3, 1, 5, 54, 20];
+const teste = array.map((arr) => arr % 2 == 0);
+console.log(teste);
