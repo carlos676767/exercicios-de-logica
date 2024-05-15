8d820a84bb293dd8d206fb7721e22e8ee1ede1bc
carlos676767 <carlitobr730@gmail.com>
2024-05-15 08:01:50 -0700

    ok

---
+++ exercicios/463.ts
@@ -0,0 +1,16 @@
+//Explicit Type Casting
+
+//temos duas maneiras um usando o <> e outro as
+
+//o as e mais usado em ambientes jsx 
+//ja o <> e mais usado na linguagem vanilla
+
+//ex 
+
+const numero: any = 225;
+const numero1: string = <string>numero;
+
+//com as 
+
+const valor: any = '114'
+const converter: string = valor as string
