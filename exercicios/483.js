0bece8a11cfb78902a9d3e6d9e28551701dcce0f
carlos676767 <carlitobr730@gmail.com>
2024-05-25 15:42:42 -0700

    ok

---
+++ exercicios/483.js
@@ -0,0 +1,14 @@
+//uma funcao genetor e uma funcao que e pausada
+function* contarVogais(nome) {
+    let contador = 0;
+    const vogais = ["a", 'e', 'i', 'o', 'u'];
+    const separarPalavra = nome.split("")
+    for (let i = 0; i < separarPalavra.length; i++) {
+        if (vogais.includes(separarPalavra[i])) {
+            ++contador
+            yield contador
+        }
+    }
+}
+const gerador = contarVogais('carlos henrique');
+console.log(gerador.next());
