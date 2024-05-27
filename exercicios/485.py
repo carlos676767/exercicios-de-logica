ff12576832d53aad4da7dbb2f72cd555a376f2ba
carlos676767 <carlitobr730@gmail.com>
2024-05-27 08:11:47 -0700

    ok

---
+++ exercicios/485.py
@@ -0,0 +1,10 @@
+def verificarIdade():
+    idade = 5
+    if idade >= 18:
+        print("adulto")
+    elif idade <= 5:
+        print("crianca")
+    else:
+        print("nao nasci")
+
+verificarIdade()
