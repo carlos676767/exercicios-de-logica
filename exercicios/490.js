642d9658fcad1eb942e71560b5d4c5bb0625c201
carlos676767 <carlitobr730@gmail.com>
2024-06-01 09:18:20 -0700

    ok

--- exercicios/490.js
+++ exercicios/490.js
@@ -1,8 +1,11 @@
+const nome = require("./491");
+
 //introducao do node js
+console.log(nome);
 
 (() => {
-  const nome = 'joao';
-  const ana = 'silva'
-  const juntar = nome.concat(ana)
-  console.log(juntar);
+    const nome = 'joao';
+    const ana = 'silva'
+    const juntar = nome.concat(ana)
+    console.log(juntar);
 })()
---
+++ exercicios/491.js
@@ -0,0 +1,9 @@
+//modules em node js
+
+const nome = {
+    idade: 18,
+    nascimento: 2004
+}
+
+
+module.exports = nome
