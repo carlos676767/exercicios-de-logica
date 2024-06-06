eb91476881ac2fe56ba1f633330a1e8d0db6631a
carlos676767 <carlitobr730@gmail.com>
2024-06-06 15:23:09 -0700

    /.

--- exercicios/510.js
+++ exercicios/510.js
@@ -1,9 +1,9 @@
 //tipos de consoles
-console.log();//imprime valores
+console.log(); //imprime valores
 console.count(10); //conta valores
-console.warn() //exibe um aviso.
-console.error() //exibe um erro
-console.debug() // faz um debug
+console.warn(); //exibe um aviso.
+console.error(); //exibe um erro
+console.debug(); // faz um debug
 
 console.time(6000);
 const tstes = {
---
+++ exercicios/511.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+console.log(chalk.red('This is an error message!'));
+console.log(chalk.green('This is a success message.'));
+console.log(chalk.bold.underline('This text is bold and underlined.'));
+console.log(chalk.bgBlue.white('This text has a blue background with white text.'));
+console.log(chalk.keyword('orange')('This text is orange')); // Using named colors
