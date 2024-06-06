ef5f53b05ccbeb15be35f17da210539d5514b354
carlos676767 <carlitobr730@gmail.com>
2024-06-06 15:01:36 -0700

    ok

---
+++ exercicios/510.js
@@ -0,0 +1,15 @@
+//tipos de consoles
+console.log();//imprime valores
+console.count(10); //conta valores
+console.warn() //exibe um aviso.
+console.error() //exibe um erro
+console.debug() // faz um debug
+
+console.time(6000);
+const tstes = {
+  nome: "carlkos",
+  idade: 18,
+  sobreNome: "bananna",
+};
+
+console.debug(tstes);
