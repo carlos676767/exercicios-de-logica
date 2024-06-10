1f01faaba97dd94d7dae8483a1b2a9f5d8dc5f49
carlos676767 <carlitobr730@gmail.com>
2024-06-10 11:20:25 -0700

    ok

---
+++ exercicios/523.js
@@ -0,0 +1,13 @@
+function sortArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] < array[j]) {
+        let temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+      }
+    }
+  }
+  return array
+}
+console.log(sortArray([9,8,7,5]));
--- exercicios/main.py
+++ exercicios/main.py
@@ -1,2 +1,10 @@
+def somar(arr):
 
+  if not arr:  
+    return 0
 
+  total_sum = 0
+  for element in arr:
+    if isinstance(element, (int, float)):  
+      total_sum += element
+  return total_sum
