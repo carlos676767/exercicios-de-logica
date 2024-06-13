9166d91ded4647862ec003aab87f9306fde4ef1a
carlos676767 <carlitobr730@gmail.com>
2024-06-13 15:08:34 -0700

    rb

--- exercicios/547.js
+++ exercicios/547.js
@@ -4,3 +4,16 @@ function ola() {
   console.log("ola mundo");
 }
 ola();
+
+class Pessoa {
+  constructor(nome, idade, peso) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+  }
+  executar() {
+    console.log(`${this.nome} tem idade ${this.idade} e peso ${this.peso}`);
+  }
+}
+
+const pessoa = new Pessoa("carlos", "18", "1.98");
---
+++ exercicios/549.rb
@@ -0,0 +1,13 @@
+class Pessoa 
+  def initialize(nome, idade,peso)
+    @nome = nome
+    @idade = idade
+    @peso = peso
+  end
+  def acao()
+    puts "#{@nome} tem idade #{@idade} e tem peso #{@peso}"
+  end
+end
+
+nova_pessoa = Pessoa.new("carlos", "20", "1.78")
+nova_pessoa.acao()
