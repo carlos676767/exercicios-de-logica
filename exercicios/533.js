08fbba8d30cd41863097a299e8f8fcbfd68f033b
carlos676767 <carlitobr730@gmail.com>
2024-06-13 08:58:51 -0700

    estrutura de dados

---
+++ exercicios/533.js
@@ -0,0 +1,22 @@
+//estrutura de dados
+
+
+//array e basicamente uma lista
+const arr = [1,1,5,9,254]
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);  
+}
+//pilha o ultimo elemento a entrar e o primeiro a sair.
+
+const array = [1,2,3]
+array.push(10)
+const pilha = array.pop()
+
+
+//fila o primeiro elemento a entrar e o primeiro a sair.
+
+const fila = [1,2,9,8,5,6,4,6]
+fila.unshift(100)
+fila.shift(100)
+console.log(fila);
+
