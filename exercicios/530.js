7c0ca87b713476c8e61367d8c6e4f7ce5e1a3f2f
carlos676767 <carlitobr730@gmail.com>
2024-06-13 08:20:07 -0700

    tipos de dados revisao

---
+++ exercicios/530.js
@@ -0,0 +1,15 @@
+//tipos de dados revisao
+
+//numeros inteiros
+const numeroInteiro = 4
+console.log(numeroInteiro);
+//numero flutuante 
+const numeroFlutuante = 5.4
+console.log(numeroFlutuante);
+//boleanos
+const eVerdadeiro = true
+const eFalse = false
+console.log(eVerdadeiro, eFalse);
+//strings
+const str = 'carlos'
+console.log(str);
