e83f6a352b89873c190738e860cbfdd818d3ad2b
carlos676767 <carlitobr730@gmail.com>
2024-06-13 08:32:31 -0700

    operadores e condicionais

---
+++ exercicios/531.js
@@ -0,0 +1,25 @@
+//- operadores e condicionais
+
+//operador de mais
+const somar = 5 + 5;
+console.log(somar);
+console.log();
+//multiplicar
+const multiplicar = 5 * 5;
+console.log(multiplicar);
+//operador de menos
+const retirar = 6 - 1;
+console.log(retirar);
+//ordem de precedencia
+const ordemDePrecendia = (5 / 2) * 1;
+console.log(ordemDePrecendia);
+
+const idade = 8;
+// condicionais
+if (idade >= 18) {
+  console.log("maior de idade");
+} else if (idade >= 1 && idade <= 0) {
+  console.log("sou crianca");
+} else {
+  console.log("adolecente");
+}
