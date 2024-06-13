c3e7d78f24f1f10e3280f5ff19f1fde1356123ee
carlos676767 <carlitobr730@gmail.com>
2024-06-13 09:27:42 -0700

    ok

---
+++ exercicios/534.js
@@ -0,0 +1,14 @@
+//oque sao uma funcao ?
+//e um bloco reuitiliaval
+
+//chamada
+//um processamento
+// um paran
+//retorno
+function somar(a, b) {
+  const multiplicar = a * b;
+  return multiplicar;
+}
+
+console.log(somar(5, 5));
+console.log(somar(2, 5));
