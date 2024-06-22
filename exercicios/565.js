7aad02c0b3c2c24784c8725088e4ad592494fda1
carlos676767 <carlitobr730@gmail.com>
2024-06-21 18:33:12 -0700

    node js modulo core os ver as informacoes do pc

---
+++ exercicios/565.js
@@ -0,0 +1,15 @@
+//os module
+
+const os = require("os")
+//ver a versao do win
+console.log(os.version());
+//host
+console.log(os.hostname());
+//ver as cpus
+console.log(os.cpus());
+//updatetime
+console.log(os.uptime());
+//plataforma
+console.log(os.platform());
+//ram
+console.log(os.totalmem());
