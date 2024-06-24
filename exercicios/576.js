16e4e373cc019e43d642b496946e23a0b578ff20
carlos676767 <carlitobr730@gmail.com>
2024-06-24 09:07:49 -0700

    exercicio

---
+++ exercicios/576.js
@@ -0,0 +1,17 @@
+// Dado um array não vazio de inteiros,
+// retorne o resultado da multiplicação de
+// todos valores juntos em ordem. Exemplo:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x) {
+    let somar = 0
+    for (let i = 0; i < x.length; i++) {
+        somar *= x[i]
+    }
+    return somar
+}
+
+
+// console.log(grow([1, 2, 3, 4]));
+
