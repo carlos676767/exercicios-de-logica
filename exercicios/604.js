1835559657878ef6b33ffcbe6aadc8db411b8427
carloshigorbr12 <carlitobr730@yahoo.com>
2024-07-05 17:47:15 -0400

    novos ex

---
+++ exercicios/604.js
@@ -0,0 +1,19 @@
+//Substituir Todas as Ocorrências de uma Substring:
+
+//Escreva uma função que substitui todas as 
+//ocorrências de uma substring por outra substring em uma 
+//string maior.
+
+
+//Substituir Todas as Ocorrências de uma Substring:
+
+//Escreva uma função que substitui todas as 
+//ocorrências de uma substring por outra substring em uma 
+//string maior.
+function ocorrenciaSubString(str1) {
+    return 'era uma vez uma pessoa chamada joana e gostava de pao'
+    .replace('joana', str1)
+ }
+ 
+ 
+ console.log(ocorrenciaSubString('silva'))
