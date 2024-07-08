3f0b467e732008bcee1102f5ea73dc0603d8397e
carlos silva <139733392+carlos676767@users.noreply.github.com>
2024-07-07 23:39:00 -0400

    novos ex

---
+++ exercicios/618.js
@@ -0,0 +1,8 @@
+const converterEmMaiscula = (palavra) => {
+  const splitPalavra = palavra.split(" ")
+  return splitPalavra.map(data => data.charAt().toUpperCase() + data.slice(1)).join(" ")
+}
+
+console.log(converterEmMaiscula("ola me chamo carlos"))
+
+console.log(converterEmMaiscula("o ana juliaaa"))
