a404ed16e81e5f74b94b106d04e7920efb8a3688
carlos676767 <carlitobr730@gmail.com>
2024-07-10 13:28:41 -0700

    ok

--- exercicios/635.js
+++ exercicios/635.js
@@ -1,10 +1,10 @@
 function cifrar(str, decifre) {
   const alfabeto = Array.from("abcdefghijklmnopqrstuvwxyz"); //ele avanca cada letra h + 2 = k
-  str.toLowerCase().trim()
+  str.toLowerCase().trim();
   const newStr = str.split("");
   const newArr = [];
   for (let i = 0; i < newStr.length; i++) {
-    let temp = alfabeto.indexOf(newStr[i]) + decifre % 26;
+    let temp = alfabeto.indexOf(newStr[i]) + (decifre % 26);
     newArr.push(alfabeto.at(temp));
   }
   return newArr.join("");
