1ad45190e73f97e01385fa1d52793ccbe636a0a1
carlos676767 <carlitobr730@gmail.com>
2024-07-22 17:06:59 -0700

    novos ex

---
+++ exercicios/682.js
@@ -0,0 +1,20 @@
+function basicOp(operation, value1, value2) {
+    let result = 0;
+    switch (operation) {
+      case "+":
+        result = value1 + value2;
+        break;
+      case "-":
+        result = value1 - value2;
+        break;
+        case "*":
+        result = value1 * value2
+        break
+        case "/":
+        result = value1 / value2
+        break
+      default:
+        break;
+    }
+    return result;
+  }
