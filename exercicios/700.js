d5115659dd7b13b1bfa5b99f72855490e6de28b7
carlos676767 <carlitobr730@gmail.com>
2024-07-25 16:05:23 -0700

    ok

--- exercicios/700.js
+++ exercicios/700.js
@@ -1,8 +1,7 @@
-const reverseSeq = n => {
-    const arr = []
-     for(let i =n; i>= 1; --i){
-       arr.push(i)
-    
-    }
-    return arr;
-  };
+const reverseSeq = (n) => {
+  const arr = [];
+  for (let i = n; i >= 1; --i) {
+    arr.push(i);
+  }
+  return arr;
+};
---
+++ exercicios/701.js
@@ -0,0 +1,26 @@
+function findOutlier(integers) {
+  const arr = [];
+  const arr2 = [];
+  for (let i = 0; i < integers.length; i++) {
+    if (integers[i] % 2 !== 0) {
+      arr.push(integers[i]);
+    }
+  }
+
+  for (let i = 0; i < integers.length; i++) {
+    if (integers[i] % 2 === 0) {
+      arr2.push(integers[i]);
+    }
+  }
+
+  if (arr.length === 1) {
+    return Number(arr);
+  }
+
+  if (arr2.length == 1) {
+    return Number(arr2);
+  }
+
+}
+
+console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
