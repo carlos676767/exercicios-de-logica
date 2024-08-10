76a9ae648bb8ea53b79396b50589fc20ed17a6d4
carlos676767 <carlitobr730@gmail.com>
2024-08-09 18:36:22 -0700

    novos ex

---
+++ exercicios/750.js
@@ -0,0 +1,16 @@
+function sortArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] % 2 !== 0 && array[j] % 2 !== 0) {
+        if (array[i] < array[j]) {
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+      }
+    }
+  }
+  return array;
+}
+
+console.log(sortArray([5, 8, 6, 3, 4])); // [3, 8, 6, 5, 4]
