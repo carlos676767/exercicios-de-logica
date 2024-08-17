f35213223c5f731290275454bc03cbf86a45074e
carlos676767 <carlitobr730@gmail.com>
2024-08-17 01:16:40 -0700

    novos ex

---
+++ exercicios/778.js
@@ -0,0 +1,17 @@
+function duplicateCount(text) {
+  const ignorarLetras = text.toLowerCase().split("");
+  const contarPlavras = {};
+  ignorarLetras.forEach((data) => {
+    if (!contarPlavras[data]) {
+      contarPlavras[data] = 1;
+    } else {
+      ++contarPlavras[data];
+    }
+  });
+  const arr = Object.values(contarPlavras).filter((data) => data > 1).length;
+  return arr;
+}
+
+console.log(duplicateCount("aabbcde"));
+
+
