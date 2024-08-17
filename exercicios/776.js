ea182d6fe4c6632e38ae3e97ef2e9c8ac2995cfd
carlos676767 <carlitobr730@gmail.com>
2024-08-17 00:57:15 -0700

    novos ex

---
+++ exercicios/776.js
@@ -0,0 +1,5 @@
+function whatCentury(year) {
+    const seculo = (Math.floor(year / 100) + 1)
+    return seculo
+}
+
---
+++ exercicios/777.js
@@ -0,0 +1,5 @@
+function swapValues(arr) {
+  var temp = arr[0];
+  arr[0] = arr[1];
+  arr[1] = temp;
+}
