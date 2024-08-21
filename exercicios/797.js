0e25f625ba4bcfe77e3eff8d63dc1c1847166fec
carlos676767 <carlitobr730@gmail.com>
2024-08-21 14:24:31 -0700

    novos ex

---
+++ exercicios/797.js
@@ -0,0 +1,17 @@
+function nicknameGenerator(name) {
+    if (name.length < 4) {
+      return "Name too short";
+    }
+    
+    const vogais = ["a", "e", "i", "o", "u"];
+    const quartaLetra = name[3];
+  
+    if (!vogais.includes(quartaLetra)) {
+      return name.slice(0, 4);
+    } 
+    
+     return name.slice(0, 3);
+  }
+  
+console.log(nicknameGenerator("Robert"));
+console.log(nicknameGenerator("Jeannie"));
