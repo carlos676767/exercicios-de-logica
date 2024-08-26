ab708ca31e02fdb3dfdb20ddc53a9f587d72d0dd
carlos676767 <carlitobr730@gmail.com>
2024-08-26 14:48:12 -0700

    novos ex

--- exercicios/817.js
+++ exercicios/817.js
@@ -5,7 +5,7 @@ function XO(str) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] == "o") {
       ++countO;
-    } else if(arr[i] == "x") {
+    } else if (arr[i] == "x") {
       ++countS;
     }
   }
