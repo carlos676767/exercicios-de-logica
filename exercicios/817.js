599eb220f4dd18d1460c54e2a2c1ffe1cc973b46
carlos676767 <carlitobr730@gmail.com>
2024-08-26 14:45:30 -0700

    novo ex

---
+++ exercicios/817.js
@@ -0,0 +1,15 @@
+function XO(str) {
+  const arr = str.toLowerCase().split("");
+  let countO = 0;
+  let countS = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "o") {
+      ++countO;
+    } else if(arr[i] == "x") {
+      ++countS;
+    }
+  }
+  return countO === countS ? true : false;
+}
+
+console.log(XO("ooxx"));
