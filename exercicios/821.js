bb3f7bdbc51ab68dd385ad812f686c9b8a14d96f
carlos676767 <carlitobr730@gmail.com>
2024-08-27 22:40:06 -0700

    novos ex

---
+++ exercicios/821.js
@@ -0,0 +1,15 @@
+function high(sentence) {
+  const alphabet = Array.from(Array(26).keys()).map((index) => String.fromCharCode(index + 97) );
+
+  const words = sentence.split(" ").map((word) => word.split(""));
+
+  const wordScores = words.map((letters) => letters.map((letter) => alphabet.indexOf(letter) + 1) 
+  .reduce((acc, score) => acc + score) );
+
+  const highestScoringWord = wordScores.indexOf(Math.max(...wordScores))
+  const acess = words[highestScoringWord].join("")
+  return acess;
+}
+
+console.log(high("man i need a taxi up to ubud"));
+
