df419912abec256cbdfac7a120db200d600f9182
carlos676767 <carlitobr730@gmail.com>
2024-09-02 14:30:04 -0700

    novos ex

---
+++ exercicios/829.js
@@ -0,0 +1,5 @@
+const quarterOf = (month) => {
+  return Math.ceil(month / 3)
+};
+
+module.exports = quarterOf;
---
+++ exercicios/test/06.test.js
@@ -0,0 +1,13 @@
+const quarterOf = require("../829")
+
+test("Quarter of the year", () => {
+    expect(quarterOf(3)).toBe(1)
+})
+
+
+
+test("Quarter of the year", () => {
+    expect(quarterOf(8)).toBe(3)
+})
+
+
