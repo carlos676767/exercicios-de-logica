6f3f9ffc525fe7b008419b96b7e4bcd9b3fc8362
carlos676767 <carlitobr730@gmail.com>
2024-09-06 15:46:50 -0700

    novos ex

---
+++ exercicios/833.js
@@ -0,0 +1,8 @@
+function minMax(arr) {
+  const arrayOrder = arr.sort((a, b) => a - b);
+  if (arrayOrder.length === 1) {
+    return [arrayOrder[ultimoIndice], arrayOrder[ultimoIndice]];
+  }
+  const ultimoIndice = arrayOrder.length - 1;
+  return [arrayOrder[0], arrayOrder[ultimoIndice]];
+}
