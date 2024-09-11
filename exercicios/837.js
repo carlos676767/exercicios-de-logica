80c31d567763a18cc4a399263f79b3e54abd30d4
carlos676767 <carlitobr730@gmail.com>
2024-09-10 23:16:00 -0700

    novo ex

--- exercicios/837.js
+++ exercicios/837.js
@@ -4,5 +4,5 @@ function twoSort(s) {
   const list = s.sort()
   return list[0].split("").join("***")
 }
-// 'b***i***t***c***o***i***n'
+
 console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]));
