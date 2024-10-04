31e6013fac27934fe46ba0d53061b5c3008beee9
carlos676767 <carlitobr730@yahoo.com>
2024-10-04 01:33:24 -0300

    novos ex

---
+++ exercicios/883.js
@@ -0,0 +1,15 @@
+function toCamelCase(str){
+    const obterPrimeirLetra = str[0]
+    
+    const values = str.toLowerCase()
+    .replace(/[_-]/g, " ").split(' ')
+    .map(data => data.charAt(0).toUpperCase() + data.slice(1)).join('')
+  
+    const primeiraLetraMaiscula = values[0]
+    return values.replace(primeiraLetraMaiscula, obterPrimeirLetra)
+  }
+
+  console.log(toCamelCase("the-stealth-warrior"))
+
+
+  //https://www.codewars.com/kata/517abf86da9663f1d2000003/train/javascript
