fe60cd7e9728617ffa6b41d313250d876f754b24
carlos676767 <carlitobr730@yahoo.com>
2024-10-08 01:27:12 -0300

    novos metodos

---
+++ exercicios/890.js
@@ -0,0 +1,26 @@
+class AgruparValores {
+  constructor(array, objeto) {
+    this.array = array;
+    this.objeto = objeto;
+  }
+
+  agruparArray() {
+    return Map.groupBy(this.array, (arr) =>
+      arr < 0 ? "negativo" : "positivo"
+    )
+  }
+
+   agruparObjetos() {
+    return Object.groupBy(this.objeto, (obejct) => obejct)
+  }
+}
+
+const valoresAgrupar = new AgruparValores(
+  [1, 9, 8, 6, 8, -1, 8, -88, 2],
+  [1, 9, 8, 6, 8, -1, 8, -88, 2],
+);
+
+console.log(valoresAgrupar.agruparArray());
+
+console.log(valoresAgrupar.agruparObjetos());
+
