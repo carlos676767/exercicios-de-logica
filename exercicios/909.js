f4e8d7847d5ceff03ab1cf2bb8137f828538e93e
carlos676767 <carlitobr730@yahoo.com>
2024-10-22 19:23:04 -0300

    novo ex

---
+++ exercicios/909.js
@@ -0,0 +1,10 @@
+
+function humanReadable(seconds) {
+    const hours = Math.floor(seconds / 3600).toString().padStart(2, '0');
+    const minutes = Math.floor((seconds % 3600) / 60).toString().padStart(2, '0');
+    const segundos = (seconds % 60).toString().padStart(2, '0'); 
+    return `${hours}:${minutes}:${segundos}`;
+  }
+  
+  
+  
