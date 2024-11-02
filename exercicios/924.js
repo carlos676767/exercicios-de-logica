e384cca4ffc33495065cb9de36bd16efc355a96c
carlos676767 <carlitobr730@yahoo.com>
2024-11-02 18:33:54 -0300

    novos exercicios, fazendo apenas para nao perder o foco.

---
+++ exercicios/924.js
@@ -0,0 +1,15 @@
+const palavraMaisLonga = (palavra) => {
+  const myPalavra = palavra.split(" ");
+  const obterNumeros = myPalavra.map((data) => data.length);
+  const obterMaior = Math.max(...obterNumeros);
+
+  const indice = obterNumeros.indexOf(obterMaior);
+
+  return myPalavra.at(indice);
+};
+
+console.log(
+  palavraMaisLonga(
+    "Esta é uma frase de exemplo para encontrar a palavra mais longa"
+  )
+);
