e148baad7b8a905ef5915c7e4d7b2d19918e43ae
carlos676767 <carlitobr730@yahoo.com>
2024-11-03 14:30:44 -0300

    novos exercicios, fazendo apenas para nao perder o foco.

--- exercicios/940.js
+++ exercicios/940.js
@@ -1,5 +1,4 @@
 const numeroAlto = (valor) => {
-    const order = valor.sort((acc, ac) => acc - ac)
-    return order[order.length - 1]
-  }
-  
+  const order = valor.sort((acc, ac) => acc - ac);
+  return order[order.length - 1];
+};
