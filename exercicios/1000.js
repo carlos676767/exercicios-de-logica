2fc1d5146cd452afe1826bc1e16389fe50ff92c4
carlos676767 <carlitobr730@yahoo.com>
2024-11-11 15:21:04 -0300

    em comemoracao ao exercicio 1000 fiz um timer.

---
+++ exercicios/1000.js
@@ -0,0 +1,24 @@
+class Timer {
+  static timer = document.getElementById("timer");
+  static myTimer() {
+    let temp = 3600000;
+    const timerNow = setInterval(() => {
+      temp -= 1000;
+      Timer.configsTemp(temp);
+      if (temp <= 0) {
+        clearInterval(timerNow);
+        document.title = "O tempo de 1h acabou.";
+      }
+    }, 1000);
+  }
+
+  static configsTemp(temp) {
+    const minutes = Math.floor(temp / 60000);
+    const seconds = Math.floor((temp % 60000) / 1000);
+    let tempoTotal = `${String(minutes).padStart(2, "0")}:${String( seconds  ).padStart(2, "0")}`;
+    this.timer.innerText = tempoTotal;
+    document.title = tempoTotal;
+  }
+}
+
+Timer.myTimer();
