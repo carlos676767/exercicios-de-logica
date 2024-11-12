8ede31061ce083c8dccc878204381a9225158166
carlos676767 <carlitobr730@yahoo.com>
2024-11-12 17:02:49 -0300

    exercicios de logica.

---
+++ exercicios/1003.js
@@ -0,0 +1,8 @@
+
+
+
+function createPhoneNumber(numbers) {
+    const dd = '(' + numbers.slice(0, 3).join('') + ')'
+    return dd + ' ' + numbers.slice(3, 6).join('') + '-' + numbers.slice(6, 10).join('')
+};
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
