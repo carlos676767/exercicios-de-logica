2d188e2cd65622ab71692c26da4c85f804d9c668
carlos676767 <carlitobr780@yahoo.com>
2024-12-15 17:27:42 -0400

    novo ex

---
+++ exercicios/1019.js
@@ -0,0 +1,28 @@
+function encode(string) {
+    const vogais = {
+      a: 1,
+      e: 2,
+      i: 3,
+      o: 4,
+      u: 5
+    };
+  
+    return [
+      ...string
+    ].map(char => vogais[char] || char).join(``)
+  }
+  
+  console.log(encode(`hello`))
+  function decode(string) {
+    const vogais = {
+      1: 'a',
+      2: 'e',
+      3: 'i',
+      4: 'o',
+      5: 'u'
+    };
+  
+    return [
+      ...string
+    ].map(char => vogais[char]  || char).join(``)
+  }
