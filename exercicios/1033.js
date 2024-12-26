ceb3e8e3a1ad8c25c9fc600250bc3a213322e1b8
carlos676767 <carlitobr780@yahoo.com>
2024-12-26 18:29:46 -0400

    novos ex

---
+++ exercicios/1033.js
@@ -0,0 +1,7 @@
+function sortArray(array) {
+  const order = array.filter((char) => char % 2 !== 0).sort((a, b) => a - b);
+
+  let indicex = 0;
+
+  return array.map((char) => (char % 2 !== 0 ? order[indicex++] : char));
+}
