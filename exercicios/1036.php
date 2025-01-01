5a5a5b90969c4ce6358bbae6aa7b7d9d5d4d612a
carlos676767 <carlitobr780@yahoo.com>
2025-01-01 11:52:41 -0400

    novo ex

---
+++ exercicios/1036.php
@@ -0,0 +1,13 @@
+<?php
+
+  class InverterString
+ {
+    static public function inverterStr(string $str){
+        $strReversed = strrev($str);
+        return $strReversed;
+    }
+ }
+ 
+
+ echo InverterString::inverterStr('Hello World!'); 
+?>
