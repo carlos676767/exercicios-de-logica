ba9cd0f22d9204be7f946c7d64624eee8a166759
carlos676767 <carlitobr780@yahoo.com>
2025-01-03 21:16:44 -0400

    novos ex

---
+++ exercicios/1041.php
@@ -0,0 +1,19 @@
+
+<?php
+
+
+final class Positivesum
+{
+    static public function positive_sum($arr): int
+    {
+        $filterFilter =  array_filter($arr, function ($char) {
+            return $char > 0;
+        });
+        
+        return array_reduce($filterFilter, function($a, $b){
+            return $a + $b;
+        }, 0);
+    }
+}
+
+echo Positivesum::positive_sum([1, -4, 7, 12]);
