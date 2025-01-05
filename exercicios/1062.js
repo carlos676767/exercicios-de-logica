d98d5e1831d91d6dc503bd6bfc0a7e7e5f111d65
carlos676767 <carlitobr780@yahoo.com>
2025-01-05 17:22:31 -0400

    novos ex

---
+++ exercicios/1062.js
@@ -0,0 +1,7 @@
+const dontGiveMeFive = (start, end) => {
+  const numbers = Array.from(
+    { length: end - start + 1 },
+    (_, index) => start + index
+  );
+  return numbers.filter((char) => char !== 5).length;
+};
