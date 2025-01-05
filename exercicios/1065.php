b1cf33b8d8702034c7e06e5afc85382d8bdcea0d
carlos676767 <carlitobr780@yahoo.com>
2025-01-05 18:06:37 -0400

    novos ex

---
+++ exercicios/1065.php
@@ -0,0 +1,18 @@
+<?php
+
+final class IsIsogram
+{
+    static public function isIsogram($string) : bool 
+    {
+        if (!$string) {
+            return true;
+        }
+
+       $string = strtolower($string);
+       $strSplit = str_split($string);
+      return count(array_unique($strSplit)) === strlen($string);
+    }
+}
+
+
+print_r(IsIsogram::isIsogram("Dermatoglyphics"));
