54abdf4c790a2bd51e8c485560366a2e91111875
carlos676767 <carlitobr780@yahoo.com>
2025-01-07 12:09:31 -0400

    novo ex

--- exercicios/1068.js
+++ exercicios/1068.js
@@ -1,14 +1,14 @@
-function stray(numbers) {
-    const count ={
-  
-    }
+class Stray {
+  static stray(numbers) {
+    const count = {};
     numbers.forEach((char) => {
-    if (!count[char]) {
-      count[char] = 1; 
-    } else {
-      count[char]++; 
-    }
-  });
-  return Number(Object.keys(count).find(chars => count[chars] === 1))
-  };
-  
+      if (!count[char]) {
+        count[char] = 1;
+      } else {
+        count[char]++;
+      }
+    });
+    const values = Number(Object.keys(count).find((chars) => count[chars] === 1));
+    return values > 0 ? values : null;
+  }
+}
