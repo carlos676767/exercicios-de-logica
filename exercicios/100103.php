9d63e285eab10d8a99053f9a3271702a93e7dfcf
carlos676767 <carlitobr780@yahoo.com>
2025-01-13 20:45:25 -0400

    ok

---
+++ exercicios/100103.php
@@ -0,0 +1,18 @@
+<?php
+
+final class FindAEvenAndOdd
+{
+    static public  function find($integers) {
+        $even = array_filter($integers, function($value){
+            return $value %2 == 0;
+        });
+
+        $odd = array_filter($integers, function($value){
+            return $value %2 !== 0;
+        });
+
+        return count($even) === 1 ? reset($even) :  reset($odd);
+    }
+}
+
+print_r(FindAEvenAndOdd::find([2, 4, 0, 100, 4, 11, 2602, 36]));
