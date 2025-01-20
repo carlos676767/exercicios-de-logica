e5962d476266677e9086a30037a4df55d9960eaf
carlos676767 <carlitobr780@yahoo.com>
2025-01-20 16:47:17 -0400

    novo ex

---
+++ exercicios/100115.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+class fakeBin
+{
+    static public function fakeBin(string $input)
+    {
+        $strSepare = str_split($input);
+
+        $strFakeBinConversion =  $strSepare = array_map(function ($char) {
+            return $char < "5" ? "0" : "1";
+        }, $strSepare);
+        
+        return implode("", $strFakeBinConversion);
+    }
+}
+
+
+
+print_r(fakeBin::fakeBin("45385593107843568"));
+
