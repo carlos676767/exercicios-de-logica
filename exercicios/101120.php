07f1f59c3305b056a03478b09fb87396eacda4fb
carlos676767 <carlitobr780@yahoo.com>
2025-01-23 16:24:58 -0400

    http get php

---
+++ exercicios/101120.php
@@ -0,0 +1,13 @@
+<?php
+
+$response = file_get_contents('https://jsonplaceholder.typicode.com/posts');
+
+$data = json_decode($response, true);
+
+$bodies = array_column($data, 'body');
+
+sort($bodies);
+
+print_r($bodies);
+
+?>
