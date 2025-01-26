ab4b0ecbc323d0bcf87717d5a6446917304a2e7c
carlos676767 <carlitobr780@yahoo.com>
2025-01-26 19:20:56 -0400

    ok

--- exercicios/101129.js
+++ exercicios/101129.js
@@ -13,3 +13,5 @@ function high(x) {
   
   
   console.log(high('man i need a taxi up to ubud'))
+
+
---
+++ exercicios/101130.php
@@ -0,0 +1,56 @@
+<?php
+
+
+final  class Classhigh
+{
+    static public function high(string $str){
+        $alphabet = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z'
+        ];
+        
+        $strInArr = self:: strInArray($str);
+        $strStrings = self:: valuesString($str);
+        return $strInArr;
+    }
+    
+    private static function strInArray(string $str){
+      $strInArray = [];
+      $currentWord = '';
+
+        for ($i = 0; $i < strlen($str); $i++) {
+            if ($str[$i] !== ' ') {
+                $currentWord .= strtolower($str[$i]);
+            } else {
+                $strInArray[] = $currentWord;
+                $currentWord = '';
+            }
+        }
+        
+        if ($currentWord !== '') {
+            $strInArray[] = $currentWord;
+        }
+
+        return $strInArray;
+
+    }
+    
+    
+    private static  function valuesString(array $list){
+      $arrayValues = [];
+      
+        for ($i = 0; $i < count($list); $i++) {
+         
+        for ($j = 0; $j < count($list); $j++) {
+         
+        }
+        }
+        
+        return $arrayValues;
+    }
+}
+
+print_r(Classhigh::high('man i need a taxi up to ubud'));
+
+
