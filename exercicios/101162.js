54ea88e1eca5e378db3a5850f46708c47bfb2ec8
carlos676767 <carlitobr780@yahoo.com>
2025-02-11 17:01:17 -0400

    novo ex

---
+++ exercicios/101162.js
@@ -0,0 +1,8 @@
+String.prototype.camelCase=function(){
+    return this.split(` `).map((word, index) => {
+      index === 0
+          ? word.toLowerCase() 
+          : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    }).join(` `)
+  }
+  
