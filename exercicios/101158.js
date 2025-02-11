9ac110d3ca88e8d7ee575662311fae100b87d09c
carlos676767 <carlitobr780@yahoo.com>
2025-02-11 16:01:27 -0400

    novo ex

--- exercicios/101158.js
+++ exercicios/101158.js
@@ -4,3 +4,6 @@ function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
   const num = list.map((char) => char * char).reduce((a, b) => a + b, 0);
   return Math.floor(Math.sqrt(num) / 2);
 }
+
+
+console.log(predictAge(10, 20, 30, 40, 50, 60, 70, 80)); // Output: 35
