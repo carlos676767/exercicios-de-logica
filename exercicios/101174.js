ec76feb23f5e24e09728d983663d12d8642a73cf
carlos676767 <carlitobr780@yahoo.com>
2025-02-18 11:21:28 -0400

    novo ex

--- exercicios/101174.js
+++ exercicios/101174.js
@@ -1,3 +1,25 @@
 function arrayDiff(a, b) {
     return  a.filter(n => !b.includes(n) ) 
 }
+
+function testes(n){
+    return new Promise((sucess, reject) => {
+      if(n > 0){
+        return sucess(`ok`)
+      }
+  
+      reject(`reject value`)
+    })
+  }
+  
+  
+  (async() => {
+    try {
+      const testes =  await testes(2)
+      console.log(testes);
+      
+    } catch (error) {
+        console.log(error);
+        
+    }
+  })()
---
+++ exercicios/101175.js
@@ -0,0 +1,18 @@
+function testes(n) {
+  return new Promise((sucess, reject) => {
+    if (n > 0) {
+      return sucess(`ok`);
+    }
+
+    reject(`reject value`);
+  });
+}
+
+(async () => {
+  try {
+    const testes = await testes(2);
+    console.log(testes);
+  } catch (error) {
+    console.log(error);
+  }
+})();
