1af1f4527ca59ec757ee9abf9308afd4c8948448
carlos676767 <carlitobr780@yahoo.com>
2025-02-25 17:24:28 -0400

    novo ex

--- exercicios/101183.js
+++ exercicios/101183.js
@@ -1,9 +1,13 @@
 String.prototype.camelCase = function () {
-  return this.split(` `)
-    .map((char) => {
-      return char[0].toUpperCase().concat(char.slice(1));
-    })
-    .join(``);
-};
 
-console.log("hello case".camelCase());
+    return this.split(` `)
+    
+      .map((char) => {
+  
+        return char.charAt(0).toUpperCase().concat(char.slice(1));
+  
+      })
+      .join(``);
+  
+  };
+  
