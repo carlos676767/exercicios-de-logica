5133659291e00ba8bfd74e56cf94a5bc7281c342
carlos676767 <carlitobr780@yahoo.com>
2025-02-25 17:21:02 -0400

    novo ex

--- exercicios/101183.js
+++ exercicios/101183.js
@@ -1,11 +1,9 @@
 String.prototype.camelCase = function () {
-    
   return this.split(` `)
     .map((char) => {
-      return char[0].toUpperCase() + char.slice(1);
+      return char[0].toUpperCase().concat(char.slice(1));
     })
     .join(``);
 };
-
 
 console.log("hello case".camelCase());
