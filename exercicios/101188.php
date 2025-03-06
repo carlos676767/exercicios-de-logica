cebb5a55cdfbff3f2823c654397102fdcd32155b
carlos676767 <carlitobr780@yahoo.com>
2025-03-06 15:01:27 -0400

    novo ex

---
+++ exercicios/101188.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+final class Palindromo
+{
+    private string $str;
+
+    public function __construct(string $str)
+    {
+        $this->str = $str;
+    }
+    
+    public function isPalindromo() : string{
+     $string = strrev($this -> str);
+     return $string === $this -> str ? "É um palíndromo" :"Não é um palíndromo";
+    }
+}
+
+
+$palindromo = new Palindromo("ovo");
+echo($palindromo -> isPalindromo());
