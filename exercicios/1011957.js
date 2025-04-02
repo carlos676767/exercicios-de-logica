efbbe843cd655d7e0391c26f2809bb4dadc291e5
carlos676767 <carlitobr780@yahoo.com>
2025-04-02 18:21:32 -0400

    novo ex

---
+++ exercicios/1011957.js
@@ -0,0 +1,7 @@
+function encodeEmail(email) {
+    const splitStr = email.split(`@`);
+    const atEmail =  `@` + splitStr[1];
+    const resultValueName = splitStr[0].split(``).map(char => char = `*`).join(``)
+    return resultValueName + atEmail
+  }
+  
