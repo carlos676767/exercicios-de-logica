e4d3165d0ea5095cbb97573da783067f53b804bc
carlos676767 <carlitobr780@yahoo.com>
2025-04-10 19:37:19 -0400

    novo exx

---
+++ exercicios/1011106.js
@@ -0,0 +1,15 @@
+const upperCase = (str) => {
+  let value = ``;
+  for (let i = 0; i < str.length; i++) {
+    const num = str[i].charCodeAt();
+    if (num >= 97 && num <= 122) {
+      value += String.fromCharCode(num - 32);
+    } else {
+      value += str[i];
+    }
+  }
+
+  return value;
+};
+
+console.log(upperCase(`carlos silva`));
