c926fb9185f40de5cc206da80e6565be5bf8dabe
carlos676767 <carlitobr780@yahoo.com>
2025-04-14 20:22:17 -0400

    novo ex

--- exercicios/1011109.php
+++ exercicios/1011109.php
@@ -1,15 +1,14 @@
 <?php
-function countBits( int $n) 
+function countBits(int $n)
 {
     $binary = decbin($n);
     $arr = str_split($binary);
-    $arr = array_filter($arr, function($char){
+    $arr = array_filter($arr, function ($char) {
         return $char === "1";
     });
-    
-    
+
+
     return count($arr);
 }
 
 var_dump(countBits(1234));
-?>
