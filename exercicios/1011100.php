6515f6c52f35d49df8d65021fb160a03c4e9ecc9
carlos676767 <carlitobr780@yahoo.com>
2025-04-14 21:25:15 -0400

    novo ex

--- exercicios/1011100.php
+++ exercicios/1011100.php
@@ -1,12 +1,10 @@
 <?php
 
-
 function detect_pangram($string) { 
-      $str =  preg_replace('/[^a-z]/', '', strtolower($string));
+    $str =  preg_replace('/[^a-z]/', '', strtolower($string));
     
     $arr = str_split($str);
     $unique = array_unique($arr);
     
     return count($unique) == 26;
 }
-  
---
+++ exercicios/101112.php
@@ -0,0 +1,12 @@
+<?php
+  
+function getCount(string $str) {
+  $strSplit = str_split($str);
+  $vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  
+  $list = array_filter($strSplit,function($char)use($vogais){
+      return in_array($char, $vogais);
+  });
+  
+  return count($list);
+}
