dc7eabdb8fd9ce862418dc61d291b3d64a3d4104
carlos676767 <carlitobr780@yahoo.com>
2025-04-15 21:36:23 -0400

    novo ex

---
+++ exercicios/1011918.php
@@ -0,0 +1,33 @@
+
+<?php
+
+
+function moveZeros(array $items)
+{
+    $list = [];
+    $removeO = [];
+
+    $listConcat = [];
+
+    for ($i = 0; $i < count($items); $i++) {
+        if ($items[$i] === 0) {
+            array_push($list, $items[$i]);
+        } else {
+            array_push($removeO, $items[$i]);
+        }
+    }
+
+    foreach ($removeO as $vector) {
+        array_push($listConcat, $vector);
+    }
+
+    foreach ($list as $vector) {
+        array_push($listConcat, $vector);
+    }
+
+    
+    return $listConcat;
+}
+
+
+print_r(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
