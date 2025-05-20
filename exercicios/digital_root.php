fa5124320b6843ba44ff388a399b6ec04eedc966
carlos676767 <carlitobr780@yahoo.com>
2025-05-19 20:54:58 -0400

    novo ex

---
+++ exercicios/digital_root.php
@@ -0,0 +1,23 @@
+<?php
+function digital_root($number)
+{
+    $str =  (string) ($number);
+
+
+    $str = str_split($str);
+
+
+    $str = array_map(function ($char) {
+        return (int) $char;
+    }, $str);
+
+
+    $soma = array_reduce($str, function ($a, $b) {
+        return $a + $b;
+    }, 0);
+
+    return $soma;
+}
+
+
+var_dump(digital_root(942));
