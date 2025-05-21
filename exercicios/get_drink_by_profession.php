4e1f883ab86c8812d4fb7741ae28698116246ede
carlos676767 <carlitobr780@yahoo.com>
2025-05-20 21:38:16 -0400

    novo ex

---
+++ exercicios/get_drink_by_profession.php
@@ -0,0 +1,17 @@
+<?php
+
+function get_drink_by_profession(string $s) {
+    $drinkByProfession = [
+    "Jabroni" => "Patron Tequila",
+    "School Counselor" => "Anything with Alcohol",
+    "Programmer" => "Hipster Craft Beer",
+    "Bike Gang Member" => "Moonshine",
+    "Politician" => "Your tax dollars",
+    "Rapper" => "Cristal"
+];
+
+return $drinkByProfession[$s] ?? "Beer" ;
+}
+
+
+var_dump(get_drink_by_profession("o"));
