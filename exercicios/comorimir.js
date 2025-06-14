f9e413575077bb3ea89910b8c35f5982b4dff52e
carlos676767 <carlitobr780@yahoo.com>
2025-06-13 20:45:48 -0400

    novo ex

---
+++ exercicios/comorimir.js
@@ -0,0 +1,24 @@
+const comorimir = (char) => {
+    const countObject = {
+  
+    }
+  
+    for (let i = 0; i < char.length; i++) {
+      if (!countObject[char[i]]) {
+        countObject[char[i]] = 1
+      } else {
+        ++countObject[char[i]]
+      }
+    }
+  
+    const uniqueKeys = Array.from(new Set(char))
+   
+    const valuesCountys = uniqueKeys.map(myStr=> {
+      return myStr + countObject[myStr]
+    }).join(``)
+  
+    
+  
+    return valuesCountys.length >= char.length ? char : valuesCountys
+  }
+  
