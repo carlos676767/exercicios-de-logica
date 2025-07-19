8038c87406636f28a848c66ad15b7164bf19d11e
carlos676767 <carlitobr780@yahoo.com>
2025-07-18 20:59:43 -0400

    novo ex

---
+++ exercicios/validePa.php
@@ -0,0 +1,20 @@
+<?php
+
+function validarPa( string $str){
+    $list = [];
+
+    for ($i=0; $i < strlen($str); $i++) { 
+        if($str[$i] === "("){
+            array_push($list, $str[$i]);
+        }else if($str[$i] === ")"){
+            if(!$list){
+               return false;
+            }
+            array_pop($list);
+        }
+    }
+    return count($list) === 0 ? true : false;
+}
+
+var_dump(validarPa(("(2+3)*(4-1)")));
+?>
