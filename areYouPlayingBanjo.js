d7b1d1f54deaead89080412353ea2546eb8bdfff
carlos676767 <carlitobr780@yahoo.com>
2026-05-04 10:31:14 -0400

    new ex

--- areYouPlayingBanjo.js
+++ areYouPlayingBanjo.js
@@ -1,8 +1,9 @@
 function areYouPlayingBanjo(name) {
-    const firstLetter = name[0].toLowerCase()
-    const letter = "r"
-    return firstLetter === letter ? `${name} plays banjo` : `${name} does not play banjo`
-  }
-  
-  console.log(areYouPlayingBanjo("rdam"));
-  
+  const firstLetter = name[0].toLowerCase();
+  const letter = "r";
+  return firstLetter === letter
+    ? `${name} plays banjo`
+    : `${name} does not play banjo`;
+}
+
+console.log(areYouPlayingBanjo("rdam"));
