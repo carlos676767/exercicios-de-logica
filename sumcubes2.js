cd417622d4d037500d3b6675dc835ef57efbb941
carlos676767 <carlitobr780@yahoo.com>
2026-05-07 10:03:36 -0400

    new ex

---
+++ sumcubes2.js
@@ -0,0 +1,10 @@
+function sumCubes(n) {
+  return Array.from({ length: n }, (_, i) => i + 1).reduce(
+    (acc, interatopn) => {
+      return acc + Math.pow(interatopn, 3);
+    },
+    0
+  );
+}
+
+console.log(sumCubes(9.99999999999999999));
