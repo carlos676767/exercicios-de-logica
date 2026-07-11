44d3b83429a634006ba127bd36e0c27e4d8c8bad
carlos676767 <carlitobr780@yahoo.com>
2026-07-11 15:51:00 -0400

    novo ex

--- fibo.js
+++ fibo.js
@@ -1,16 +1,15 @@
 function productFib(prod) {
-    let f1 = 0;
-    let f2 = 1;
-  
-    while (f1 * f2 < prod) {
-      let next = f1 + f2;
-      f1 = f2;
-      f2 = next;
-    }
-  
-    const result = f1 * f2;
-    return [f1, f2, result == prod];
+  let f1 = 0;
+  let f2 = 1;
+
+  while (f1 * f2 < prod) {
+    let next = f1 + f2;
+    f1 = f2;
+    f2 = next;
   }
-  
-  console.log(productFib(4895));
-  
+
+  const result = f1 * f2;
+  return [f1, f2, result == prod];
+}
+
+console.log(productFib(4895));
