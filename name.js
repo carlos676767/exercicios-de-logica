df5302215ecd8cfed3fdab168e3afdd857d2ed19
carlos676767 <carlitobr780@yahoo.com>
2026-07-11 16:03:28 -0400

    novo ex

---
+++ name.js
@@ -0,0 +1,15 @@
+
+
+function hello(name) {
+ 
+    if (!name) {
+      return "Hello, World!";
+    }
+  
+  
+  name =  name.toLowerCase();
+     return `Hello, ${name[0]. toUpperCase()}${name.slice(1)}!`
+  }
+  
+  console.log(hello(`carlos`));
+  
