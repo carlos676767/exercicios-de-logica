fd855760f9d48e3fa00d575ce79e4200fd7a1e1e
carlos676767 <carlitobr780@yahoo.com>
2026-07-17 18:04:58 -0400

    novo ex

--- isUppecase2.js
+++ isUppecase2.js
@@ -1,16 +1,15 @@
 String.prototype.isUpperCase = function () {
-    let count = 0;
-  
-    const upperCaseStr = this.toUpperCase();
-    for (let i = 0; i < this.length; i++) {
-      if (this[i] === upperCaseStr[i]) {
-        count++;
-      }
+  let count = 0;
+
+  const upperCaseStr = this.toUpperCase();
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === upperCaseStr[i]) {
+      count++;
     }
-  
-    return count === this.length;
-  };
-  
-  console.log("HELLO".isUpperCase());
-  console.log("Hello".isUpperCase());
-  
+  }
+
+  return count === this.length;
+};
+
+console.log("HELLO".isUpperCase());
+console.log("Hello".isUpperCase());
--- name.js
+++ name.js
@@ -13,3 +13,6 @@ function hello(name) {
   
   console.log(hello(`carlos`));
   
+
+
+
